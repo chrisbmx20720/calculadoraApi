83cc02c6a0174270539565cbf4316271a31f9f12
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-05-04 22:24:41 -0600

    arreglo de atributos en modelo

--- api/models/Puesto.js
+++ api/models/Puesto.js
@@ -1,18 +1,20 @@
-// models/Puesto.js
 const mongoose = require('mongoose');
 
 const PuestoSchema = new mongoose.Schema({
-  nombre: String,
+  nombre: { type: String, required: true },
   cantidades: {
     dia1: { type: Number, default: 0 },
     dia2: { type: Number, default: 0 },
     dia3: { type: Number, default: 0 },
   },
-  agregado: Number,
-  perdidas: Number,
-  devolucion: Number,
-  porcentaje: Number,
-  total: String
+  agregado: { type: Number, default: 0 },
+  perdidas: { type: Number, default: 0 },
+  devolucion: { type: Number, default: 0 },
+  porcentaje: { type: Boolean, default: false },
+  total: { type: String, default: '0.00' }, // puede ser Number si prefieres
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Puesto', PuestoSchema);
+
